68eccb71673ffe62522ac933d1869d71a6e8be6d
Vatan Srivastava <vatansrivastava999@gmail.com>
2023-06-28 09:43:15 +0530

    [Feat]:fix Dark mode #4

--- components/layout/site-header.tsx
+++ components/layout/site-header.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import MainNav from "./main-nav";
 import { navConfig } from "@/config/nav";
 import { Button } from "../ui/button";
+import { ModeToggle } from "../mode-toggle";
 
 type Props = {};
 
@@ -10,6 +11,7 @@ export default function SiteHeader({}: Props) {
     <header className="sticky top-0 z-40 w-full bg-background/95 backdrop-blur p-3">
       <div className="container flex h-14 items-center md:gap-6 gap-14">
         <MainNav items={navConfig} />
+        <ModeToggle />
         <Button className="rounded-full">Mentorship</Button>
       </div>
     </header>
