348fc57b876cd30bbd01f720546154b12fc7d1b8
Rohit Gurav <thisisrohitgurav@gmail.com>
2023-06-29 22:26:53 +0530

    Added Footer section

--- components/layout/site-footer.tsx
+++ components/layout/site-footer.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Button } from "../ui/button";
 
 type Props = {};
 
 export default function SiteFooter({}: Props) {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-gray-200 py-10">
+      <div className="container mx-auto px-4">
+        <div className="flex items-center justify-center">
+          <a href="/" className="text-gray-700 hover:text-gray-900 mr-4">
+            Home
+          </a>
+          <a href="/about" className="text-gray-700 hover:text-gray-900 mr-4">
+            About
+          </a>
+          <Button className="rounded-full">Mentorship</Button>
+          <a
+            href="/features"
+            className="text-gray-700 hover:text-gray-900 mr-4 ml-4"
+          >
+            Features
+          </a>
+          <a href="/join" className="text-gray-700 hover:text-gray-900 mr-4">
+            Join
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
