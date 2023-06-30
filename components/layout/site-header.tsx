7c9449fca8a988c083e15ebda2196475ae6b4ab3
aishwarya-mali <aishwaryamali29@gmail.com>
2023-06-30 11:43:39 +0530

    updated spacing for mobile devices

--- components/layout/site-header.tsx
+++ components/layout/site-header.tsx
@@ -5,10 +5,10 @@ import { Button } from "../ui/button";
 
 type Props = {};
 
-export default function SiteHeader({}: Props) {
+export default function SiteHeader({ }: Props) {
   return (
     <header className="sticky top-0 z-40 w-full bg-background/95 backdrop-blur p-3">
-      <div className="container flex h-14 items-center md:gap-6 gap-14">
+      <div className="container flex h-14 items-center md:gap-6 gap-14 md:px-8 px-0">
         <MainNav items={navConfig} />
         <Button className="rounded-full">Mentorship</Button>
       </div>
