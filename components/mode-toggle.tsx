10e4bf4e962f6d3d33f1e1e4cd05a605555d5ec4
Sujit <mnamesujit@gmail.com>
2023-07-12 22:33:02 +0530

    added padding around theme switcher

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -17,7 +17,7 @@ export function ModeToggle() {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild className="h-8">
+      <DropdownMenuTrigger asChild className="h-8 px-1.5 py-4">
         <Button variant="outline" size="icon">
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
