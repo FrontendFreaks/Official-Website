0fbbf15658695269f2c91772370ef9fb59f20b37
Tanmay Joshi <tanmayjoshi072@gmail.com>
2023-10-07 13:20:18 +0000

    Hidden text Is Visible in the DOCS section

--- app/(docs)/docs/[[...slug]]/page.tsx
+++ app/(docs)/docs/[[...slug]]/page.tsx
@@ -36,7 +36,7 @@ export default async function page({ params }: Props) {
       </div>
       <div className="hidden text-sm xl:block">
         <div className="sticky top-16 -mt-10 max-h-[calc(var(--vh)-4rem)] overflow-y-auto pt-10">
-          table of contents
+       
         </div>
       </div>
     </main>
