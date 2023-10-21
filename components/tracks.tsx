3ecdcb4616e717fcd5da924db69d2ea2c468156a
Monis Anees <u7462031@anu.edu.au>
2023-10-21 23:16:35 +1100

    Commit Work 1. Trying to fix Deployment (Hover Effect only) -----------------------------------------------

--- components/tracks.tsx
+++ components/tracks.tsx
@@ -3,7 +3,6 @@ import { buttonVariants } from "./ui/button";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 
-
 type Props = {};
 
 export default function Tracks({}: Props) {
@@ -14,40 +13,98 @@ export default function Tracks({}: Props) {
             </h4>
             <p>Master the Frontend Development with Frontend Freaks</p>
 
+            {/* Cards Wrapper */}
             <div className="flex gap-6 mt-8 md:flex-row flex-col">
-                <BatchCard imageSrc="/learn_img.svg" title="Build Your Foundation" description="Make your foundation strong by learning HTML, CSS, JS, Git, and GitHub. Join this batch if you want to make your foundation as strong as concrete." href="/batch/learn/html/basic" />
-                <BatchCard imageSrc="/dsa_in_javascript.svg" title="DSA In Javascript" description="Enroll for this batch to ace DSA skills with JavaScript, enhancing your skills for job success." href="/batch/dsa/loops" />
-                <BatchCard imageSrc="/build_img.svg" title="Build Projects" description="Build the project with the latest technologies like React, Redux, and Next.js. Join this batch if you want to be a part of it." href="/batch/build/react/fundamentals" />
-                <BatchCard imageSrc="/hire_img.svg" title="Get Hired" description="Enroll for this batch to ace interviews with tips, tricks, and mock sessions, enhancing your skills for job success." href="/batch/hire/hire" />
+                <div className="flex flex-col gap-3 items-center border p-4 rounded-lg group hover:scale-105 transition-transform">
+                    <Image
+                        src="/learn_img.svg"
+                        alt="learn"
+                        width={250}
+                        height={250}
+                        className="rounded bg-white"
+                    />
+                    <div className="flex flex-col gap-2 flex-grow">
+                        <h3 className="text-xl font-bold ">Build Your Foundation</h3>
+                        <p className="text-md text-slate-600 max-w-md dark:text-white">
+                            Make your foundation strong by learning HTML, CSS, JS, Git, and
+                            GitHub. Join this batch if you want to make your foundation as
+                            strong as concrete.
+                        </p>
+                    </div>
+                    <Link
+                        href="/batch/learn/html/basic"
+                        className={cn(buttonVariants({ variant: "rounded" }), "px-8 py-2 mt-4")}
+                    >
+                        Join
+                    </Link>
+                </div>
+                <div className="flex flex-col gap-3 items-center border p-4 rounded-lg group hover:scale-105 transition-transform">
+                    <Image
+                        src="/dsa_in_javascript.svg"
+                        alt="learn"
+                        width={250}
+                        height={250}
+                        className="rounded bg-white"
+                    />
+                    <div className="flex flex-col gap-2 flex-grow">
+                        <h3 className="text-xl font-bold ">DSA In Javascript</h3>
+                        <p className="text-md text-slate-600 max-w-md dark:text-white">
+                            Enroll for this batch to ace DSA skills with javascript, enhancing
+                            your skills for job success.
+                        </p>
+                    </div>
+                    <Link
+                        href="/batch/dsa/loops"
+                        className={cn(buttonVariants({ variant: "rounded" }), "px-8 py-2 mt-4")}
+                    >
+                        Join
+                    </Link>
+                </div>
+                <div className="flex flex-col gap-3 items-center border p-4 rounded-lg group hover:scale-105 transition-transform">
+                    <Image
+                        src="/build_img.svg"
+                        alt="learn"
+                        width={250}
+                        height={250}
+                        className="rounded bg-white"
+                    />
+                    <div className="flex flex-col gap-2 flex-grow">
+                        <h3 className="text-xl font-bold ">Build Projects</h3>
+                        <p className="text-md text-slate-600 max-w-md dark:text-white">
+                            Build the project with the latest technologies like React, Redux,
+                            and Next.js. Join this batch if you want to be a part of it.
+                        </p>
+                    </div>
+                    <Link
+                        href="/batch/build/react/fundamentals"
+                        className={cn(buttonVariants({ variant: "rounded" }), "px-8 py-2 mt-4")}
+                    >
+                        Join
+                    </Link>
+                </div>
+                <div className="flex flex-col gap-3 items-center border p-4 rounded-lg group hover:scale-105 transition-transform">
+                    <Image
+                        src="/hire_img.svg"
+                        alt="learn"
+                        width={250}
+                        height={250}
+                        className="rounded bg-white"
+                    />
+                    <div className="flex flex-col gap-2 flex-grow">
+                        <h3 className="text-xl font-bold ">Get Hired</h3>
+                        <p className="text-md text-slate-600 max-w-md dark:text-white">
+                            Enroll for this batch to ace interviews with tips, tricks, and
+                            mock sessions, enhancing your skills for job success.
+                        </p>
+                    </div>
+                    <Link
+                        href="/batch/hire/hire"
+                        className={cn(buttonVariants({ variant: "rounded" }), "px-8 py-2 mt-4")}
+                    >
+                        Join
+                    </Link>
+                </div>
             </div>
         </section>
-    );
-}
-
-function BatchCard({ imageSrc, title, description, href }) {
-    return (
-        <div className="flex flex-col gap-3 items-center border p-4 rounded-lg group hover:scale-105 transition-transform">
-            <a href={href}>
-                <img
-                    src={imageSrc}
-                    alt="learn"
-                    width={250}
-                    height={250}
-                    className="rounded bg-white batch-card-image"
-                />
-            </a>
-            <div className="flex flex-col gap-2 flex-grow">
-                <h3 className="text-xl font-bold">{title}</h3>
-                <p className="text-md text-slate-600 max-w-md dark:text-white">
-                    {description}
-                </p>
-            </div>
-            <Link
-                href={href}
-                className={cn(buttonVariants(), "rounded-full px-8 py-2 mt-4")}
-            >
-                Join
-            </Link>
-        </div>
     );
 }
